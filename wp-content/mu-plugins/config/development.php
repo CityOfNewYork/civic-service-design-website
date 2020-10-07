5deabeb536b42c1c76f43ce2d934aa7c5a3e181d
devowhippit <devonhirth@gmail.com>
2020-10-07 14:26:55 -0400

    Add Query Monitor logging to debug method

--- wp-content/mu-plugins/config/development.php
+++ wp-content/mu-plugins/config/development.php
@@ -2,11 +2,15 @@
 
 /**
  * Development environment config
+ *
+ * @author NYC Opportunity
  */
 
 /**
  * Whoops PHP Error Handler
+ *
  * @link https://github.com/filp/whoops
+ *
  * @author NYC Opportunity
  */
 
@@ -17,25 +21,32 @@ if (class_exists('Whoops\Run')) {
 }
 
 /**
- * Shorthand for debug logging
- * @author NYC Opportunity
+ * Shorthand for debug logging. Supports native debug log and query monitor
+ * logging.
  *
  * @param   String   $str     The string to log.
  * @param   Boolean  $return  Wether to make it human readable.
+ *
+ * @author NYC Opportunity
  */
 // phpcs:disable
 function debug($str, $return = true) {
   $backtrace = debug_backtrace()[0];
 
-  error_log(
-    var_export($str, $return) . " " .
-    $backtrace['file'] . ':' . $backtrace['line']
-  );
+  $file = isset($backtrace['file']) ? $backtrace['file'] . ':' : '';
+  $line = isset($backtrace['line']) ? $backtrace['line'] : '';
+
+  // Sent log to native debug.log
+  error_log(var_export($str, $return) . " " . $file . $line);
+
+  // Send log to Query Monitor
+  do_action('qm/debug', var_export($str, $return));
 }
 // phpcs:enable
 
 /**
  * Include the plugins module
+ *
  * @author NYC Opportunity
  */
 
@@ -43,6 +54,7 @@ require_once ABSPATH . 'wp-admin/includes/plugin.php';
 
 /**
  * Disable Limit Login Attempts plugin
+ *
  * @author NYC Opportunity
  */
 
@@ -52,6 +64,7 @@ deactivate_plugins([
 
 /**
  * Reset Defender 2fa settings
+ *
  * @author NYC Opportunity
  */
 
@@ -61,15 +74,15 @@ update_option('wd_2auth_settings', '');
  * Enable the Redis Caching Plugin if we have WP_REDIS_HOST defined in
  * the wp-config.php. Caching will optimize the speed of the site, especially
  * transient caches.
+ *
  * @author NYC Opportunity
  */
 
-if (null !== WP_REDIS_HOST) {
-  activate_plugin('redis-cache/redis-cache.php');
-}
+activate_plugin('redis-cache/redis-cache.php');
 
 /**
- * Enable Query Monitor for advanced Wordpress Query debug and other tooling.
+ * Enable Query Monitor for advanced WordPress Query debug and other tooling
+ *
  * @author NYC Opportunity
  */
 
@@ -77,6 +90,7 @@ activate_plugin('query-monitor/query-monitor.php');
 
 /**
  * Activate WP Auto Login for seamless local login
+ *
  * @author NYC Opportunity
  */
 
@@ -84,13 +98,14 @@ activate_plugin('wp-auto-login/wp-auto-login.php');
 
 /**
  * Allow local development requests
+ *
  * @author NYC Opportunity
  */
 
-header('Access-Control-Allow-Origin: *');
+// header('Access-Control-Allow-Origin: *');
 
-add_filter('allowed_http_origins', function($origins) {
-  $origins[] = 'http://localhost:7000'; // Patterns
+// add_filter('allowed_http_origins', function($origins) {
+//   $origins[] = 'http://localhost:7000'; // Patterns
 
-  return $origins;
-});
+//   return $origins;
+// });
