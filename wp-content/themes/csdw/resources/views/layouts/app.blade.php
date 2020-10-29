e1ce38fb2c3cf2e5f9bf1ac078250f29d6010c5c
devowhippit <devonhirth@gmail.com>
2020-10-29 16:11:36 -0400

    Add theme support for the open body tag function

--- wp-content/themes/csdw/resources/views/layouts/app.blade.php
+++ wp-content/themes/csdw/resources/views/layouts/app.blade.php
@@ -2,13 +2,18 @@
 <html {!! get_language_attributes() !!}>
   @include('partials.global.head')
   <body @php body_class() @endphp>
+    @php wp_body_open() @endphp
+
     @php do_action('get_header') @endphp
+
     @include('partials.global.header')
+
     <div role="document">
       <div class="content">
         <main class="main">
           @yield('content')
         </main>
+
         @if (App\display_sidebar())
           <aside class="sidebar">
             @include('partials.global.sidebar')
@@ -16,8 +21,11 @@
         @endif
       </div>
     </div>
+
     @php do_action('get_footer') @endphp
+
     @include('partials.global.footer')
+
     @php wp_footer() @endphp
   </body>
 </html>
